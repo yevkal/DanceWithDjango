3ec2f731d777043af82d9b5271a99989c852bbe4
yevkal <kaliuzhnyiyevhenii@gmail.com>
2023-01-18 22:21:25 +0300

    Add route converters

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -1,17 +1,7 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('leo/', leo),
-    path('scorpio/', scorpio),
-    path('aries/', aries),
-    path('taurus/', taurus),
-    path('gemini/', gemini),
-    path('cancer/', cancer),
-    path('virgo/', virgo),
-    path('libra/', libra),
-    path('sagittarius/', sagittarius),
-    path('capricorn/', capricorn),
-    path('aquarius/', aquarius),
-    path('pisces/', pisces),
+    path('<int:sign_zodiac>/', views.get_info_about_sign_zodiac_by_numbers),
+    path('<str:sign_zodiac>/', views.get_info_about_sign_zodiac),
 ]
